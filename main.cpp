5f53180c441d2e1e05995e608c008f803ae8fed0
SSead <smailagicsead@gmail.com>
2020-04-29 14:16:30 +0200

    :bug: comitpush, commit and push working!

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 std::string x = "";
 
@@ -8,19 +9,29 @@ void submodule(std::vector<std::string>&);
 void commitpush(std::vector<std::string>&);
 void commit(std::vector<std::string>&);
 void push(std::vector<std::string>&);
+void commitpushnewbranch(std::vector<std::string>&);
 
 
 main(int argc, char **argv) {
+
+    //Creating a vector of all the argments
     std::vector<std::string> arg;
     for (int i = 0; i < argc; i++) arg.push_back(*(argv + i));
 
+
+    //Checking if there are no aditional arguments
     if (argc == 1) {
+
+        //Run welcome page
         welcome();
     } else {
+
+        //Run the submodule program
         submodule(arg);
     }
 }
 
+//Welcome page
 void welcome() {
     std::cout 
         << "Welcome to the submodule runner. This libray helps you manage submodule with git\n"
@@ -32,18 +43,40 @@ void welcome() {
 
 void submodule(std::vector<std::string>& arg) {
 
+    //Checking for the first argument
     if (arg[1] == "commitpush") {
+
+        //Commit push
         commitpush(arg);
+
     } else if (arg[1] == "commit") {
+
+        //Commit
         commit(arg);
+
     } else if (arg[1] == "push") {
+
+        //Push
         push(arg);
+
+    } else if (arg[1] == "commitpushnewbranch") {
+
+        std::cout << "WIP\n";
+        //commitpushnewbranch(arg);
+
+    } else if (arg[1] == "--help" || arg[1] == "-h") {
+
+        welcome();
+
+    } else {
+
+        //Unrecognized parametar
+        std::cout << "Parametar does not exist.\n";
     }
 }
 
 void commitpush(std::vector<std::string>& arg) {
     if (arg.size() == 2) {
-
         std::cout << "error: Missing paramater. Add --help to paramaters.\n";
 
     } else if (arg[2] == "-m"){
@@ -55,13 +88,10 @@ void commitpush(std::vector<std::string>& arg) {
                 << "    --m <commit-message>\n";
 
         } else {
-            system("git submodule foreach \"git add . \"");
-            system("git add .");
-            system((x + "git submodule foreach \"git commit -m '" + arg[3] + "' -a \"").c_str());
-            system((x + "git commit -m \"" + arg[3] + "\"").c_str());
-            system("git submodule foreach \"git push origin master\"");
-            system("git push origin master");
-            
+            system((x + "git submodule foreach \"git add --all && git commit -m '" + arg[3] + "' && git push\"").c_str());
+            system("git add --all");
+            system(("git commit -m \"" + arg[3] + "\"").c_str());
+            system("git push");
         }
         
     } else if (arg[2] == "--help") {
@@ -73,11 +103,10 @@ void commitpush(std::vector<std::string>& arg) {
     }
 }
 
-
 void commit(std::vector<std::string>& arg) {
     if (arg.size() == 2) {
 
-        std::cout << "error: Missing paramater. Add --help to paramaters.\n";
+        std::cout << "error: Missing paramater. Add --help to see paramaters.\n";
 
     } else if (arg[2] == "-m"){
 
@@ -88,11 +117,9 @@ void commit(std::vector<std::string>& arg) {
                 << "    --m <commit-message>\n";
 
         } else {
-            system("git submodule foreach \"git add . \"");
-            system("git add .");
-            system((x + "git submodule foreach \"git commit -m '" + arg[3] + "' -a \"").c_str());
-            system((x + "git commit -m \"" + arg[3] + "\"").c_str());
-            
+            system((x + "git submodule foreach \"git add --all && git commit -m '" + arg[3] + "'\"").c_str());
+            system("git add --all");
+            system(("git commit -m \"" + arg[3] + "\"").c_str());
         }
         
     } else if (arg[2] == "--help") {
@@ -105,6 +132,10 @@ void commit(std::vector<std::string>& arg) {
 }
 
 void push(std::vector<std::string>& arg) {
-    system("git submodule foreach \"git push origin master\"");
-    system("git push origin master");
+    system((x+"git submodule foreach \"git push\"").c_str());
+    system("git push");
+}
+
+void commitpushnewbranch(std::vector<std::string>& arg) {
+    
 }
